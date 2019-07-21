affc81e086601fd3b71eb963080937e350e3c7f9
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2019-07-21 09:34:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './config/AppNavigator'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator/>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        We have no friends!
+        <Button
+          title="Add some friends"
+          onPress={() =>
+            this.props.navigation.navigate('Friends')
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
