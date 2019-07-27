f0cf41033b88a385bee66bf2299af923f67d335c
sivaprakashDesingu <opensourcesivaprakash@gmail.com>
2019-07-27 11:11:43 +0530

    Initial page:changes with Selecting option

--- pages/LandingPage/initialDetails.js
+++ pages/LandingPage/initialDetails.js
@@ -12,6 +12,15 @@ import {Button } from 'react-native-paper';
 import { CommonCSS, initialPageCSS } from '../../Style'
 
 export  class InitialDetails extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { 
+      cookingSkill: 'Beginner',
+      isVegeterian:false
+    }
+  }
+
   static navigationOptions = {
     title: "Personalization",
     headerStyle: {
@@ -23,57 +32,93 @@ export  class InitialDetails extends React.Component {
     }
   };
 
+  userInitalInput(whichState,value){
+      whichState ==='cookingSkill' ? 
+      this.setState({cookingSkill:value}) : 
+      this.setState({isVegeterian:value})
+  }
 
   render() {
-    const { propsValue } = this.props;
-    const src = 'https://cdn3.vectorstock.com/i/1000x1000/53/27/chef-cartoon-icon-cooking-and-menu-design-vector-9495327.jpg'
     return (
       <ScrollView style={CommonCSS.flexContainer}>
         <StatusBar backgroundColor="#fa6767" barStyle="light-content" />
+
+        {/* Rate your cooking skills */}
         <View >
           <Text style={initialPageCSS.heading}>Rate your cooking skills!</Text>
         </View>
-        <TouchableOpacity style={initialPageCSS.buttons} activeOpacity={0.5}>
+        <TouchableOpacity 
+        onPress={this.userInitalInput.bind(this,'cookingSkill','Beginner')} 
+        style={
+          [initialPageCSS.buttons, 
+          this.state.cookingSkill==='Beginner' ? initialPageCSS.acitveButton : null]
+        }
+        activeOpacity={0.5}>
           <Image source={require('../../assets/images/avatar.png')} style={initialPageCSS.ImageIconStyle}/>
           <Text style={initialPageCSS.TextStyle}> Iam a Beginner </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={initialPageCSS.buttons} activeOpacity={0.5}>
+        <TouchableOpacity 
+        onPress={this.userInitalInput.bind(this,'cookingSkill','Expert')} 
+        style={
+          [initialPageCSS.buttons, 
+          this.state.cookingSkill==='Expert' ? initialPageCSS.acitveButton : null]
+        }
+        activeOpacity={0.5}>
           <Image source={require('../../assets/images/avatar.png')} style={initialPageCSS.ImageIconStyle}/>
           <Text style={initialPageCSS.TextStyle}> Iam an Expert </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={initialPageCSS.buttons} activeOpacity={0.5}>
+        <TouchableOpacity 
+        onPress={this.userInitalInput.bind(this,'cookingSkill','Professional')} 
+        style={
+          [initialPageCSS.buttons, 
+          this.state.cookingSkill==='Professional' ? initialPageCSS.acitveButton : null]
+        }
+        activeOpacity={0.5}>
           <Image source={require('../../assets/images/avatar.png')} style={initialPageCSS.ImageIconStyle}/>
           <Text style={initialPageCSS.TextStyle}> Iam a Professional </Text>
         </TouchableOpacity>
-
+        {/* Rate your cooking skills */}
+        
         {/* Are you Vegetarian */}
         <View>
           <Text style={initialPageCSS.heading}>Are you a Vegetarian!</Text>
         </View>
-        <TouchableOpacity style={initialPageCSS.buttons} activeOpacity={0.5}>
+        <TouchableOpacity 
+        onPress={this.userInitalInput.bind(this,'isVeg',false)} 
+        style={
+          [initialPageCSS.buttons, 
+          this.state.isVegeterian ? null : initialPageCSS.acitveButton]
+        }
+        activeOpacity={0.5}>
           <Image source={require('../../assets/images/avatar.png')} style={initialPageCSS.ImageIconStyle}/>
           <Text style={initialPageCSS.TextStyle}> Nope, Show me all Recipes </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={initialPageCSS.buttons} activeOpacity={0.5}>
+        <TouchableOpacity 
+        onPress={this.userInitalInput.bind(this,'isVeg',true)} 
+        style={
+          [initialPageCSS.buttons, 
+          this.state.isVegeterian ? initialPageCSS.acitveButton : null]
+        }
+        activeOpacity={0.5}>
           <Image source={require('../../assets/images/avatar.png')} style={initialPageCSS.ImageIconStyle}/>
           <Text style={initialPageCSS.TextStyle}> Yes, Hide recipes with meat</Text>
         </TouchableOpacity> 
         <View style = {initialPageCSS.getStartedBtnWrapper}>
-        <Button 
-              mode="contained"
-              theme={{
-                roundness: 5,
-                width: '90%',
-                dark: true,
-                colors: {
+          <Button 
+            mode="contained"
+            theme={{
+              roundness: 5,
+              width: '90%',
+              dark: true,
+              colors: {
                 primary: '#ec4242',
                 accent: "#ffffff",
-                }
-              }}
-              contentStyle = {{height:50}}
-              >
-              Get Started
-            </Button>
+              }
+            }}
+            contentStyle = {{height:50}}
+          >
+            Get Started
+          </Button>
         </View>
         
         {/* Are you Vegetarian */}
