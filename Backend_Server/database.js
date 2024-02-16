37d8717a3dbe856c71e3dc858edc90bba43d1998
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2024-02-16 12:18:48 +0530

    Update database.js
    update the db connection

--- Backend_Server/database.js
+++ Backend_Server/database.js
@@ -3,9 +3,7 @@ const MONGO_PASSWORD = 'admin123';
 const MONGO_HOSTNAME = 'localhost';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'cookingrecipe';
-//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-const url = 'mongodb+srv://admin:Admin123@cluster0-mvq6r.mongodb.net/cookingrecipe?retryWrites=true&w=majority'
-//'mongodb://localhost:27017/admin'
+const url = 'mongodb://localhost:27017/admin'
 module.exports = {
     url: url,
     db: MONGO_DB
